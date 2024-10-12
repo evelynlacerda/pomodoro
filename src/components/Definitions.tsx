38d8000e515421e69c47c00b89afd5d5487368c2
Evelyn Lacerda <evelyndslacerda@gmail.com>
2024-10-12 04:41:54 -0300

    chore: style Definitions component with SCSS
    Styled the Definitions.tsx component using SCSS in the _definitions.scss
    file. This change improves the component's maintainability and visual
    consistency with the overall application design.

--- src/components/Definitions.tsx
+++ src/components/Definitions.tsx
@@ -11,24 +11,70 @@ const Definitions = () => {
 				<div className="time">
 					<p>{t("Minutes")}</p>
 					<div className="time-set">
-						<input type="radio" name="duration" id="fifty-min" />
-						<label htmlFor="fifty-min">{t("15 minutes")}</label>
-						<input type="radio" name="duration" id="twenty-min" />
-						<label htmlFor="twenty-min">{t("25 minutes")}</label>
-						<input type="radio" name="duration" id="fourty-min" />
-						<label htmlFor="fourty-min">{t("45 minutes")}</label>
+						<div>
+							<input type="radio" name="duration" id="fifty-min" />
+							<label htmlFor="fifty-min">{t("15 minutes")}</label>
+						</div>
+						<div>
+							<input type="radio" name="duration" id="twenty-min" />
+							<label htmlFor="twenty-min">{t("25 minutes")}</label>
+						</div>
+						<div>
+							<input type="radio" name="duration" id="fourty-min" />
+							<label htmlFor="fourty-min">{t("45 minutes")}</label>
+						</div>
 					</div>
 				</div>
 
 				<div className="break">
-					<p>{t("Pause")}</p>
+					<p>{t("Break")}</p>
 					<div className="break-set">
-						<input type="radio" name="break-min" id="five-min" />
-						<label htmlFor="five-min">{t("5 minutes")}</label>
-						<input type="radio" name="break-min" id="ten-min" />
-						<label htmlFor="ten-min">{t("10 minutes")}</label>
-						<input type="radio" name="break-min" id="fifty-min" />
-						<label htmlFor="fifty-min">{t("15 minutes")}</label>
+						<div>
+							<input type="radio" name="break-min" id="five-min" />
+							<label htmlFor="five-min">{t("5 minutes")}</label>
+						</div>
+						<div>
+							<input type="radio" name="break-min" id="ten-min" />
+							<label htmlFor="ten-min">{t("10 minutes")}</label>
+						</div>
+						<div>
+							<input type="radio" name="break-min" id="fifty-min" />
+							<label htmlFor="fifty-min">{t("15 minutes")}</label>
+						</div>
+					</div>
+				</div>
+
+				<div className="custom">
+					<p>{t("Custom")}</p>
+					<div className="custom-set">
+						<input type="number" placeholder={`${t("Minutes")}`} />
+						<input type="number" placeholder={`${t("Break")}`} />
+					</div>
+				</div>
+
+				<div className="repeat">
+					<p>{t("Repeat")}</p>
+					<div className="repeat-set">
+						<div>
+							<input type="radio" name="repeat-value" id="repeat-one" />
+							<label htmlFor="repeat-one">1</label>
+						</div>
+						<div>
+							<input type="radio" name="repeat-value" id="repeat-two" />
+							<label htmlFor="repeat-two">2</label>
+						</div>
+						<div>
+							<input type="radio" name="repeat-value" id="repeat-three" />
+							<label htmlFor="repeat-three">3</label>
+						</div>
+						<div>
+							<input type="radio" name="repeat-value" id="repeat-four" />
+							<label htmlFor="repeat-four">4</label>
+						</div>
+						<div>
+							<input type="radio" name="repeat-value" id="repeat-five" />
+							<label htmlFor="repeat-five">5</label>
+						</div>
 					</div>
 				</div>
 			</div>
--- src/layouts/LanguageWrapper.tsx
+++ src/layouts/LanguageWrapper.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { Outlet, useParams } from "react-router-dom";
 
-export const LanguageWrapper: React.FC = () => {
+export const LanguageWrapper = () => {
 	const { lang } = useParams<{ lang: string }>();
 	const { i18n } = useTranslation();
 
-	React.useEffect(() => {
+	useEffect(() => {
 		if (lang && i18n.language !== lang) {
 			i18n.changeLanguage(lang);
 		}
