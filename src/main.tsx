3f5a5526f20d279e93ce0fc88fc969ec9446cbbc
Evelyn Lacerda <evelyndslacerda@gmail.com>
2024-10-12 03:16:54 -0300

    Initial commit
    Initial commit to set up the project structure and environment.
    This marks the beginning of the development process.

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import "../public/assets/style.scss"
+import "./lib/i18n.ts";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
