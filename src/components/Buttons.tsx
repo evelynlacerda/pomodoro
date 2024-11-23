e45f10c5fedef3c5ecc9ee99a9613d6fedc87433
Evelyn Lacerda <evelyndslacerda@gmail.com>
2024-11-23 18:34:22 -0300

    Adicionando botões

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,13 +1,17 @@
 import { useTranslation } from "react-i18next";
 
 const Buttons = () => {
-    const { t } = useTranslation()
+	const { t } = useTranslation();
 
-    return (
-        <div>
-        <button>{t("Click me")}</button>
-        </div>
-    );
-}
+	return (
+		<div className="buttons">
+			<button>{t("Start")}</button>
+            <div>
+                <button>{t("Pause")}</button>
+                <button>{t("Reset")}</button>
+            </div>
+		</div>
+	);
+};
 
 export default Buttons;
