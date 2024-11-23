14452f9a7ffcf35352839affa35a41a2a7fe34ee
Evelyn Lacerda <evelyndslacerda@gmail.com>
2024-11-23 18:27:41 -0300

    Configurando rotas

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { LanguageWrapper } from "../layouts/LanguageWrapper";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { LanguageWrapper } from "../layouts/LanguageWrapper";
 import Index from "../layouts/Index";
 
 const Router = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/:lang" element={<LanguageWrapper />}>
+				{/* <Route path="/:lang" element={<LanguageWrapper />}> */}
 					<Route path="" element={<Index />} />
-				</Route>
-				<Route path="*" element={<Navigate replace to="/en-us" />} />
-				<Route path="/" element={<Navigate replace to="/en-us" />} />
+				{/* </Route> */}
+				{/* <Route path="*" element={<Navigate replace to="/en-us" />} />
+				<Route path="/" element={<Navigate replace to="/en-us" />} /> */}
 			</Routes>
 		</BrowserRouter>
 	);
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [react()],
+    server: {
+        host: "localhost",
+        port: 5000,
+    },
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
